d00adee3333a637d59515295d37b3525b75b094c
James Matchett <30292929+JamesMatchett@users.noreply.github.com>
2018-05-07 14:20:12 +0100

    Groundwork for gravitational attraction method

--- Physics.js
+++ Physics.js
@@ -10,14 +10,31 @@ function setup(){
 
 function draw(){
 
-//get postiion of mouse on canvas
+//comment this out to toggle trails
 background(230);
 for(var i = 0; i<particles.length; i++){
   //update (accelerate in direction of mouse)
   var CurrentParticle = particles[i];
   //update velocities
-  CurrentParticle.Xspeed += (mouseX - CurrentParticle.x)/1450 + random(-.02,.02);
-  CurrentParticle.Yspeed += (mouseY - CurrentParticle.y)/1450 + random(-.02,.02);;
+  CurrentParticle.Xspeed += (mouseX - CurrentParticle.x)/1000;
+  CurrentParticle.Yspeed += (mouseY - CurrentParticle.y)/1000;
+
+ //accelerate each particle towards each other at an amount proportional to their seperation
+
+ for(var j =0;j <particles.length; j++){
+
+   var neighbourParticle = particles[j];
+
+   //add consideration for mass of a particle, merging partices?? could potentially be done
+   //i.e. instead of calculating an rf for m1 + m2 against 3rd particle m3, combine masses
+   //and treat as a single particle
+
+     CurrentParticle.Xspeed += (neighbourParticle.x - CurrentParticle.x)/400000;
+     CurrentParticle.Yspeed += (neighbourParticle.y - CurrentParticle.y)/400000;
+     CurrentParticle.Xspeed - 2;
+     CurrentParticle.Yspeed - 2;
+ }
+
 
   //if particle is off the screen, half the velocity
   if(CurrentParticle.x > width || CurrentParticle.X < 0){
@@ -28,11 +45,12 @@ for(var i = 0; i<particles.length; i++){
     CurrentParticle.Yspeed = CurrentParticle.Yspeed /2;
   }
 
+
   //update position by velocity
   CurrentParticle.x += CurrentParticle.Xspeed;
   CurrentParticle.y += CurrentParticle.Yspeed;
 
-  //redraw on screen
+
 
    //red-ness is based on x velocity, green on y
    fill(color(CurrentParticle.Xspeed * 100,CurrentParticle.Yspeed * 100,
@@ -50,14 +68,14 @@ for(var i = 0; i<particles.length; i++){
 }
 
 function AddParticles(){
-for(var i = 0; i <250; i++){
+for(var i = 0; i <50; i++){
   particles.push(new Particle());
 }
 }
 
 function Particle() {
-   this.x = 0;
-   this.y = 0;
+   this.x = 0+ random(-.02,.02);
+   this.y = 0+ random(-.02,.02);
    this.Xspeed = 0;
    this.Yspeed = 0;
 }
