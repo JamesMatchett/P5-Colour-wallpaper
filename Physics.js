3deeb2c662c85d052d1059f5f8b1809ff34e2f50
James Matchett <30292929+JamesMatchett@users.noreply.github.com>
2018-05-07 14:30:40 +0100

    Gravity update
    masses accounted for, each particle has random mass between values 1-5,
    Attraction is based on ratio of neighbouring particle to own particle's mass
    
    left clicking introduces a particle of mass 50 to test the mechanic.

--- Physics.js
+++ Physics.js
@@ -16,8 +16,8 @@ for(var i = 0; i<particles.length; i++){
   //update (accelerate in direction of mouse)
   var CurrentParticle = particles[i];
   //update velocities
-  CurrentParticle.Xspeed += (mouseX - CurrentParticle.x)/1000;
-  CurrentParticle.Yspeed += (mouseY - CurrentParticle.y)/1000;
+  CurrentParticle.Xspeed += (mouseX - CurrentParticle.x)/(500*CurrentParticle.mass);
+  CurrentParticle.Yspeed += (mouseY - CurrentParticle.y)/(500*CurrentParticle.mass);
 
  //accelerate each particle towards each other at an amount proportional to their seperation
 
@@ -29,8 +29,8 @@ for(var i = 0; i<particles.length; i++){
    //i.e. instead of calculating an rf for m1 + m2 against 3rd particle m3, combine masses
    //and treat as a single particle
 
-     CurrentParticle.Xspeed += (neighbourParticle.x - CurrentParticle.x)/400000;
-     CurrentParticle.Yspeed += (neighbourParticle.y - CurrentParticle.y)/400000;
+     CurrentParticle.Xspeed += (neighbourParticle.mass/CurrentParticle.mass)*(neighbourParticle.x - CurrentParticle.x)/70000;
+     CurrentParticle.Yspeed += (neighbourParticle.mass/CurrentParticle.mass)*(neighbourParticle.y - CurrentParticle.y)/70000;
      CurrentParticle.Xspeed - 2;
      CurrentParticle.Yspeed - 2;
  }
@@ -78,10 +78,17 @@ function Particle() {
    this.y = 0+ random(-.02,.02);
    this.Xspeed = 0;
    this.Yspeed = 0;
+   this.mass = random(1,10);
+}
+
+function SetParticle(Mass){
+  var mParticle = new Particle();
+  mParticle.mass = Mass;
+  return mParticle;
 }
 
 function mouseClicked(){
-  particles.push(new Particle());
+  particles.push(new SetParticle(100));
 }
 
 function windowResized(){
